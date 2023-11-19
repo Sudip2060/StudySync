32390b061c77cd83c6ee9354b5bb968559e07ab4
Sudip2060 <sudipghimire9844@gmail.com>
2023-11-19 17:53:44 -0500

    Login and Signup Page
    Login Page and the signup page has been created. The connection URL for the database has been defined.

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+require('dotenv').config();
+const mongoose = require('mongoose');
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.listen(process.env.PORT, () => { console.log("app is running in 5000 port") })
+
+app.get('/login', (req, res) => { res.render('login') })
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+    console.log("application is connected to Database")
+}).catch((err) => {
+    console.log(err)
+})
+
+
+
